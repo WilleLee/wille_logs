fdb3013aa70108ae91af918870267596104b6401
Wille Lee <1992season@gmail.com>
2024-01-29 18:22:09 +0900

    [FEAT] google analytics

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body suppressHydrationWarning={true} className={inter.className}>
         {children}
       </body>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID as string} />
     </html>
   );
 }
