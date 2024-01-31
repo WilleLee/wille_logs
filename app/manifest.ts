706a52ea0d21ae332b328918cad164ad555b29df
Wille Lee <1992season@gmail.com>
2024-01-31 18:25:01 +0900

    [ADD] added manifest.ts and logos

---
+++ app/manifest.ts
@@ -0,0 +1,45 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Next.js App",
+    short_name: "Next.js App",
+    description: "Next.js App",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/logos/avatar48.png",
+        sizes: "48x48",
+        type: "image/png",
+      },
+      {
+        src: "/logos/avatar72.png",
+        sizes: "72x72",
+        type: "image/png",
+      },
+      {
+        src: "/logos/avatar96.png",
+        sizes: "96x96",
+        type: "image/png",
+      },
+      {
+        src: "/logos/avatar144.png",
+        sizes: "144x144",
+        type: "image/png",
+      },
+      {
+        src: "/logos/avatar168.png",
+        sizes: "168x168",
+        type: "image/png",
+      },
+      {
+        src: "/logos/avatar192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
+  };
+}
