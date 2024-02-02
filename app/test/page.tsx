01c1ef75f9cef9213192fe4115d56e5fd58c6a64
Wille Lee <1992season@gmail.com>
2024-02-02 20:00:53 +0900

    modal test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,14 +1,25 @@
 "use client";
 
 import TransparentButton from "@/components/buttons/TransparentButton";
+import Modal from "@/components/modals/Modal";
 import cookies from "@libs/cookies";
-import React from "react";
+import React, { useState } from "react";
+import { createPortal } from "react-dom";
 
 type Props = {};
 
 export default function Page({}: Props) {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div>
+      {showModal
+        ? createPortal(
+            <Modal title="테스트 모달" handleClose={() => setShowModal(false)}>
+              hi
+            </Modal>,
+            document.body,
+          )
+        : null}
       <TransparentButton onClick={() => cookies.set("cookie1", "value", 30)}>
         set cookie 1
       </TransparentButton>
@@ -17,6 +28,9 @@ export default function Page({}: Props) {
       </TransparentButton>
       <TransparentButton onClick={() => cookies.delete("cookie1")}>
         delete cookie 1
+      </TransparentButton>
+      <TransparentButton onClick={() => setShowModal(true)}>
+        open modal
       </TransparentButton>
     </div>
   );
