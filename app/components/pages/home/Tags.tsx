b09510d5db661569799ec0aaf2f45c25af710c31
Wille Lee <1992season@gmail.com>
2024-02-05 11:41:40 +0900

    [REFACTOR] tags list, added outlined button

--- app/components/pages/home/Tags.tsx
+++ app/components/pages/home/Tags.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import LoadingFullpage from "@components/loading/LoadingFullpage";
 import useTags from "@hooks/useTags";
 import React, { AllHTMLAttributes } from "react";
 import { homeActions, useHomeDispatch } from "./HomeContextProvider";
 import { ITag } from "@models/TagModel";
+import OutlinedButton from "@/components/buttons/OutlinedButton";
 
 const Tags = () => {
   const { tags, status } = useTags();
@@ -18,35 +18,66 @@ const Tags = () => {
     <>
       {status == "success" ? (
         tags.length > 0 ? (
-          <TagsListView tags={tags} handleClickTag={handleClickTag} />
+          <TagsList tags={tags} handleClickTag={handleClickTag} />
         ) : (
           <p>no tag</p>
         )
       ) : null}
-      {status === "loading" && (
-        <LoadingFullpage alt="threads' tags are being loaded..." />
-      )}
+      {status === "loading" && <p>loading&hellip;</p>}
     </>
   );
 };
 
 export default Tags;
 
-interface TagsListViewProps extends AllHTMLAttributes<HTMLUListElement> {
+interface TagsListProps extends AllHTMLAttributes<HTMLUListElement> {
   tags: ITag[];
   handleClickTag: (tagId: string) => void;
 }
 
-function TagsListView({ tags, handleClickTag, ...props }: TagsListViewProps) {
+const TagsList = React.memo(function TagsList({
+  tags,
+  handleClickTag,
+  ...props
+}: TagsListProps) {
   return (
-    <ul {...props}>
+    <TagsListView {...props}>
       {tags.map((tag) => {
         return (
-          <li key={tag._id} onClick={() => handleClickTag(tag._id as string)}>
-            {tag.name}
-          </li>
+          <TagsItemView
+            key={tag._id}
+            handleClickTag={() => handleClickTag(tag._id as string)}
+            tag={tag}
+          >
+            <OutlinedButton>{tag.name}</OutlinedButton>
+          </TagsItemView>
         );
       })}
-    </ul>
+    </TagsListView>
   );
+});
+
+interface TagsListViewProps extends AllHTMLAttributes<HTMLUListElement> {
+  children: React.ReactNode;
 }
+
+function TagsListView({ children, ...props }: TagsListViewProps) {
+  return <ul {...props}>{children}</ul>;
+}
+
+interface TagsItemViewProps extends AllHTMLAttributes<HTMLLIElement> {
+  tag: ITag;
+  handleClickTag: (tagId: string) => void;
+}
+
+const TagsItemView = React.memo(function TagsItemView({
+  tag,
+  handleClickTag,
+  ...props
+}: TagsItemViewProps) {
+  return (
+    <li onClick={() => handleClickTag(tag._id as string)} {...props}>
+      <OutlinedButton>{tag.name}</OutlinedButton>
+    </li>
+  );
+});
