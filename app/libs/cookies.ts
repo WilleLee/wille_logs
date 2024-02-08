00e371e7bc2aed0e0ac62176843923c05c19a401
Wille Lee <1992season@gmail.com>
2024-02-08 16:50:40 +0900

    [FIX] added window type checks to cookies

--- app/libs/cookies.ts
+++ app/libs/cookies.ts
@@ -1,14 +1,17 @@
 const cookies = {
   set: (name: string, value: string, days: number) => {
+    if (typeof window === "undefined") return;
     const date = new Date();
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     const expires = `expires=${date.toUTCString()}`;
     document.cookie = `${name}=${value};${expires};path=/`;
   },
   delete: (name: string) => {
+    if (typeof window === "undefined") return;
     document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
   },
   get: (name: string) => {
+    if (typeof window === "undefined") return "";
     const cookieName = `${name}=`;
     const cookies = document.cookie.split(";");
     for (let i = 0; i < cookies.length; i++) {
