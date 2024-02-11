49efd6ee669cea446da719d47d48c077e2abbe09
Wille Lee <1992season@gmail.com>
2024-02-11 19:17:40 +0900

    [ADD] linkedin link

--- app/components/pages/wille/Profile/ProfileLinks.tsx
+++ app/components/pages/wille/Profile/ProfileLinks.tsx
@@ -2,6 +2,7 @@ import InstagramSvg from "@components/svgs/InstagramSvg";
 import ThreadsSvg from "@components/svgs/ThreadsSvg";
 import React from "react";
 import styles from "./profileLinks.module.scss";
+import LinkedinSvg from "@/components/svgs/LinkedinSvg";
 
 type Props = {};
 
@@ -12,6 +13,12 @@ export default function ProfileLinks({}: Props) {
         <a href="https://github.com/WilleLee/wille_logs">github.com/WilleLee</a>
       </p>
       <div>
+        <a
+          href="https://www.linkedin.com/in/inpyo-lee-24b9aa292/"
+          target="_blank"
+        >
+          <LinkedinSvg width="28" />
+        </a>
         <a href="https://www.instagram.com/wille_lee_reactive/" target="_blank">
           <InstagramSvg width="28" />
         </a>
