5e21046a55d8677981e3542c52471c7bb591da52
Wille Lee <1992season@gmail.com>
2024-02-11 18:05:03 +0900

    [ADD] extracontent /wille

--- app/components/pages/wille/ProjectsContent/projects.ts
+++ app/components/pages/wille/ProjectsContent/projects.ts
@@ -1,5 +1,6 @@
 import { StaticImageData } from "next/image";
 import avatar128 from "@images/avatar128.png";
+import rokSupporterLogo from "@images/rok_supporter_logo.png";
 
 export interface IProject {
   _id: string;
@@ -17,10 +18,23 @@ export const projects: IProject[] = [
     title: "wille logs...",
     imageSrc: avatar128,
     link: "https://wille-logs.vercel.app/",
+    githubLink: "https://github.com/WilleLee/wille_logs",
     techStack: ["typescript", "react", "next(app)", "swr", "vitest", "mongodb"],
     descriptions: [
       "a blog app to save daily book marks with authentication",
       "full stack application with next.js and mongodb",
+    ],
+  },
+  {
+    _id: "1",
+    title: "rok supporter",
+    imageSrc: rokSupporterLogo,
+    link: "https://github.com/WilleLee/rok-supporter",
+    githubLink: "https://github.com/WilleLee/rok-supporter",
+    techStack: ["javascript", "react", "zustand", "styled-components"],
+    descriptions: [
+      "a wep page to provide useful information and tools for the game rise of kingdoms",
+      "participated in the front-end development using react",
     ],
   },
 ];
