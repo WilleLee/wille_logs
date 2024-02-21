f430913d1510f23d7d973b39c8960ff8e45378e0
Wille Lee <1992season@gmail.com>
2024-02-21 18:04:56 +0900

    [FIX] language mode issue

--- app/components/layouts/InitLanguageMode.tsx
+++ app/components/layouts/InitLanguageMode.tsx
@@ -5,16 +5,15 @@ import { languageModeState } from "@atoms/languageModeState";
 import { useEffect } from "react";
 import cookies from "@libs/cookies";
 
-let initialLanguageMode = "en";
-
-if (typeof window !== "undefined") {
-  initialLanguageMode = cookies.get("languageMode");
-}
-
 export default function InitLanguageMode() {
   const setLanguageMode = useSetRecoilState(languageModeState);
 
   useEffect(() => {
+    let initialLanguageMode = "en";
+
+    if (typeof window !== "undefined") {
+      initialLanguageMode = cookies.get("languageMode");
+    }
     if (
       typeof initialLanguageMode === "string" &&
       (initialLanguageMode === "en" || initialLanguageMode === "ko")
