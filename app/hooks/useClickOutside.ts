44c31936b3f33f9d984d1d90352771ffa67479f3
Wille Lee <1992season@gmail.com>
2024-02-26 20:10:54 +0900

    [ADD] touchstart to useclickoutside

--- app/hooks/useClickOutside.ts
+++ app/hooks/useClickOutside.ts
@@ -2,10 +2,10 @@ import React, { useEffect } from "react";
 
 export default function useClickOutside(
   ref: React.RefObject<HTMLDivElement>,
-  handler: (event: MouseEvent) => any,
+  handler: (event: MouseEvent | TouchEvent) => any,
 ) {
   useEffect(() => {
-    function handleClickOutside(e: MouseEvent) {
+    function handleClickOutside(e: MouseEvent | TouchEvent) {
       if (ref.current && !ref.current.contains(e.target as Node)) {
         // console.log("ref current : ", ref.current);
         // console.log("event target : ", e.target);
@@ -13,8 +13,10 @@ export default function useClickOutside(
       }
     }
     document.addEventListener("click", handleClickOutside);
+    document.addEventListener("touchstart", handleClickOutside);
     return () => {
       document.removeEventListener("click", handleClickOutside);
+      document.removeEventListener("touchstart", handleClickOutside);
     };
   }, [ref, handler]);
 }
