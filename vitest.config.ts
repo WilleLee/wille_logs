b0fb50c9b56d47db7520234e919802d2d01ef517
Wille Lee <1992season@gmail.com>
2024-06-06 17:04:18 +0900

    test

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,14 +6,14 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
     environment: "jsdom",
-    // exclude: [
-    //   "**/node_modules/**",
-    //   "**/dist/**",
-    //   "**/cypress/**",
-    //   "**/.{idea,git,cache,output.temp}/**",
-    //   "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*",
-    //   "**/old/**",
-    //   "**/.next/**",
-    // ],
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output.temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*",
+      "**/old/**",
+      "**/.next/**",
+    ],
   },
 });
