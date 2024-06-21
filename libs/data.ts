7def064831638da1fb2c51b0d2389d2314c46f9d
Wille Lee <1992season@gmail.com>
2024-06-21 20:35:02 +0900

    test rollback

--- libs/data.ts
+++ libs/data.ts
@@ -1,10 +1,8 @@
 import { unstable_noStore as noStore } from "next/cache";
 import { ITag, IThread } from "./types";
 
-// const API_BASE_URL =
-//   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
-
-const API_BASE_URL = "/api";
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
 
 type FetchResult<T> = {
   data: T | null;
