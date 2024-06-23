98d784ae40ca40b618f159d38d545163aff195a4
Wille Lee <1992season@gmail.com>
2024-06-23 23:34:01 +0900

    favicon

--- app/(domain)/my/page.tsx
+++ app/(domain)/my/page.tsx
@@ -2,12 +2,14 @@ import FormButton from "@components/form-button";
 import Header from "@components/header";
 import { logout } from "@libs/actions";
 import { Metadata } from "next";
+import { unstable_noStore } from "next/cache";
 
 export const metadata: Metadata = {
   title: "마이 페이지",
 };
 
 export default function MyPage() {
+  unstable_noStore();
   return (
     <>
       <Header />
